fd6f0b0b8b6b16293a02ce49e096fa299c595bfe
NuriH <Nuriyharit@gmail.com>
2025-09-04 15:08:43 +0300

    update as of completing stage 2 in the login pt1 doc

---
+++ app/src/main/java/com/travelog/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.travelog;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // In LoginActivity.java
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login); // Make sure this matches your XML file name
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // You'll add listeners for your button and textview here
+        TextView registerLinkTextView = findViewById(R.id.link_register);
+
+
+        registerLinkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+    }
+}
